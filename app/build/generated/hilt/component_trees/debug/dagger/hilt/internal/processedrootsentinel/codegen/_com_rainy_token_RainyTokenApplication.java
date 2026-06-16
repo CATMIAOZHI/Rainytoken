3ba4954dbed61f32db160aab1c3a4f4c90f1d197
WaterRain <109326062+CATMIAOZHI@users.noreply.github.com>
2026-06-16 02:47:25 +0000

    修复.gitignore：排除app/build/产物

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_rainy_token_RainyTokenApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.rainy.token.RainyTokenApplication"
-)
-public final class _com_rainy_token_RainyTokenApplication {
-}
