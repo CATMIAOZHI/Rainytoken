87c3ea20f46e92f3ed76eabd5b420e24ed1a47d4
WaterRain <109326062+CATMIAOZHI@users.noreply.github.com>
2026-09-24 05:34:40 +0000

    release: bump to v1.6.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.rainy.token"
         minSdk = 31
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.6.4"
+        versionCode = 15
+        versionName = "1.6.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
